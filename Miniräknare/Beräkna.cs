bbd34536ffe547fa38d3b4f141ba11c55524f818
Hampasp <aspeflo27@gmail.com>
2022-09-27 14:38:52 +0200

    Add project files.

---
+++ Miniräknare/Beräkna.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniräknare
+{
+    internal class Beräkna
+    {
+        public static void Addition(string userInput)
+        {
+            
+            int plusindex = userInput.IndexOf("+");
+            string tal1Text = userInput[..plusindex];
+            string tal2Text = userInput[(plusindex + 1)..];
+            int tal1 = int.Parse(tal1Text);
+            int tal2 = int.Parse(tal2Text);
+            int summa = tal1 + tal2;
+            Console.WriteLine(userInput + " = " + summa);
+        }
+        public static void Subtraktion(string userInput)
+        {
+            int minusIndex = userInput.IndexOf("-");
+            string tal1Text = userInput[..minusIndex];
+            string tal2Text = userInput[(minusIndex + 1)..];
+            int tal1 = int.Parse(tal1Text);
+            int tal2 = int.Parse(tal2Text);
+            int summa = tal1 - tal2;
+            Console.WriteLine(userInput + " = " + summa);
+        }
+        public static void Division(string userInput)
+        {
+            int divisionIndex = userInput.IndexOf("/");
+            string tal1Text = userInput[..divisionIndex];
+            string tal2Text = userInput[(divisionIndex + 1)..];
+            int tal1 = int.Parse(tal1Text);
+            int tal2 = int.Parse(tal2Text);
+            int summa = tal1 + tal2;
+            Console.WriteLine(userInput + " = " + summa);
+        }
+    }
+}
---
+++ Miniräknare/Program.cs
@@ -0,0 +1,56 @@
+﻿
+Console.WriteLine("Välkommen till miniräknaren!");
+Console.WriteLine("Ange din ekvation!");
+int tal1 = 0;
+bool correctInput = false;
+while (!correctInput)
+{
+    Console.Write("tal 1: ");
+    correctInput = int.TryParse(Console.ReadLine(), out tal1);
+    
+}
+Console.Write("Ange operand: ");
+string operand = Console.ReadLine();
+
+int tal2 = 0;
+correctInput = false;
+while (!correctInput)
+{
+    Console.Write("tal 2: ");
+    correctInput = int.TryParse(Console.ReadLine(), out tal2);
+}
+
+switch (operand)
+{
+    case "+":
+        int summaAddition = tal1 + tal2;
+        Console.WriteLine(summaAddition);
+        break;
+    case "-":
+        int summaSubtrakion = tal1 - tal2;
+        Console.WriteLine(summaSubtrakion);
+        break;
+    case "*":
+        int produkt = tal1 * tal2;
+        Console.WriteLine(produkt);
+        break;
+    case "/":
+        int kvot;
+
+        try
+        {
+            kvot = tal1 / tal2;
+            Console.WriteLine(kvot);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Omöjligt att dividera med 0!");
+        }
+        break;
+
+    default:
+        break;
+}
+Console.ReadLine();
+
+
